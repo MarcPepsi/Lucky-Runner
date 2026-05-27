4a18d98399c50e99713d04024c99948edc18bd4c
MarcPepsi <marcpepsi347@gmail.com>
2026-05-27 22:31:04 +0100

    Lucky Runner V1.3
    J'ai terminé ma part, vivement la suite du développement. j'espère que mes pratiques ne nuiront pas les prochains

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -2,20 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Platform : MonoBehaviour
-{
-    public GameObject[] spawnPoints;
-    public GameObject[] obstacles;
-    [SerializeField] private GameObject GameManager;
-    private GameObject[] platforms;
 
-    private void Start()
-    {
-        platforms = GameManager.GetComponent<PlatformGenerator>().platforms;
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.name == "Player")
-            GameManager.GetComponent<PlatformGenerator>().generatePlatform(transform.position + new Vector3(0, 0, 100), platforms, transform.position, true);
-    }
+public enum level
+{
+    veryEasy,
+    easy,
+    normal,
+    high,
+    veryHigh
 }
+
+[CreateAssetMenu(fileName = "New Platform", menuName = "ScriptablesObjects/Platform")]
+public class Platform : ScriptableObject
+{
+    public GameObject prefab; //prefab de la plateforme
+    public float height = 20; //Hauteur de la plateforme
+    public GameObject[] spawnPoints; //Tableau de tous les point d'apparition d'ostacles sur la plateforme
+    public GameObject[] obstacles; //Tableau de tous les obstacles de la plateforme
+    public level level = level.easy; //Niveau de difficulté de la plateforme
+}
+
+
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,32 +5,66 @@ using static UnityEngine.UI.Image;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f;
     [SerializeField] float jumpForce = 3f, playerHeight;
-    RaycastHit hit;
     Rigidbody playerRB;
+    RaycastHit hit;
+    public float speed = 5f; //Variable de vitesse
+    public bool canGenerated = false;
+    int score = 0, side = 0; // Score du player / Variable qui sert à déterminer ou se trouve le joueur sur l'axe x
+
     void Start()
     {
+        //Récupération du rigidbody du player
         playerRB = GetComponent<Rigidbody>();
     }
 
 
     void Update()
     {
+        //Déplacement du player sur l'axe 'z' avec une vitesse définie
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
-        Debug.DrawRay(transform.position, Vector3.down * playerHeight / 2, Color.green);
         // Si on appuie sur Espace et que le Raycast touche le sol
         if (Input.GetKeyDown(KeyCode.Space))
             jump();
+        //Appel de la fonction 'Move()'
+        Move();
     }
 
+    //Fonction de saut du player
     void jump()
     {
         // Si le Raycast touche le sol on applique le saut en fonction de jumpForce
-        if (Physics.Raycast(transform.position, Vector3.down * playerHeight / 2, out hit,  2.1f))
+        if (Physics.Raycast(transform.position, Vector3.down * playerHeight / 2, out hit,  2.2f))
         {
             playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
+    }
+
+    //Fonction de déplacement du player sur l'axe 'x'
+    void Move()
+    {
+        Vector3 decalage = new Vector3(2.15f, 0, 0);
+
+        //Si le joueur appui sur une touche de direction et n'est pas déjà à sur cette direction, alors il va à vers cette direction
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && side > -5)
+        {
+            transform.position -= decalage;
+            side -= 5;
+        }
+        else if(Input.GetKeyDown(KeyCode.RightArrow) && side < 5)
+        {
+            transform.position += decalage;
+            side += 5;
+        }
+    }
+
+
+    //Si le player entre en collision avec une plateforme, alors il y'a génération d'une nouvelle
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Plateforme")
+            canGenerated = true;
     }
 }
